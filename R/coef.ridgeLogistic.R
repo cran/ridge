384d1f1e58a2ed31690a80b52d077c6641bcd69a
Erika Cule <erika.cule05@imperial.ac.uk>
2012-06-27 00:00:00 +0000

    version 1.0

---
+++ R/coef.ridgeLogistic.R
@@ -0,0 +1,17 @@
+## coef method for ridgeLogistic objects
+
+coef.ridgeLogistic <- function (object, all.coef = FALSE, ...) 
+{
+  if (object$Inter) {
+    Intercept <- apply(object$coef, 2, function(x) {x[1] - sum(object$xm * x[-1] / object$scales)})
+    scaledcoefs <- rbind(Intercept, apply(object$coef, 2, function(x){x[-1] / object$scales}))
+   } else {
+    scaledcoefs <- apply(object$coef, 2, function(x){x/object$scales})
+  }
+  if(object$automatic && all.coef == FALSE)
+    {
+      scaledcoefs <- scaledcoefs[,object$chosen.nPCs]
+    }
+  scaledcoefs <- t(scaledcoefs)
+  drop(scaledcoefs)
+}
